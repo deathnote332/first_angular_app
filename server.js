6ce9e83235cec3c9e2eac4b42ec25718b5b60f9b
Paul <jpinhog@indracompany.com>
2019-05-21 11:11:40 +0800

    update

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ const app = express();
 app.use(express.static(__dirname + '/dist/ng-first-app'));
 
 app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/ng-first-app/index.html'));
+    res.sendFile(path.join(__dirname+'/dist/ng-first-app/index.html'));
 });
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
